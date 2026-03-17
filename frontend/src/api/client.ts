d18d226ee3e728f7f12e92cf0a80c738470bf7c9
Lau <pachecolau27@gmail.com>
2026-03-17 13:08:31 -0500

    fix: making changes to assure fetching successful request to the deployed api

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.PROD
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "/api",
   headers: { "Content-Type": "application/json" },
 });
 
+console.log(import.meta.env.PROD);
 export default api;
