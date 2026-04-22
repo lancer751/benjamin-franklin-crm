e447e7add46de33e13039bacc21339625cea9562
Lau <pachecolau27@gmail.com>
2026-04-22 17:05:11 -0500

    second try

--- management-dashboard/src/App.tsx
+++ management-dashboard/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { benjaminCrmApi } from "./lib/apiConnection";
 import { createUserSchema, type CreateUserDTO } from "shared";
 
