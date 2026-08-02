5ec52950bbc110f5932f22001399e3b72333ebe3
angel <angel@email.com>
2026-08-01 21:52:25 -0500

    se actualizo TaskBaseSchema, y las migraciones

--- packages/db/prisma/migrations/20260731165044_move_task_creator_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Tasks" DROP CONSTRAINT "Tasks_created_by_fkey";
-
--- AddForeignKey
-ALTER TABLE "Tasks" ADD CONSTRAINT "Tasks_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
